784ba07739993602275558be578cd6068cf10762
Kevin Lin <kevin@quilt.io>
2017-08-05 14:46:49 -0700

    blueprint: Lint all files
    The previous linting commit linted the repository according to Google's
    style guide, not AirBnB's. This commit also adds the eslint
    configuration file so that it's easy to lint the repo in the future.

--- mean.js
+++ mean.js
@@ -1,23 +1,23 @@
-const {createDeployment, Machine, Range, githubKeys, LabelRule, publicInternet}
+const { createDeployment, Machine, Range, githubKeys, LabelRule, publicInternet }
   = require('@quilt/quilt');
-let haproxy = require('@quilt/haproxy');
-let Mongo = require('@quilt/mongo');
-let Node = require('@quilt/nodejs');
+const haproxy = require('@quilt/haproxy');
+const Mongo = require('@quilt/mongo');
+const Node = require('@quilt/nodejs');
 
 // AWS
-let namespace = createDeployment();
+const namespace = createDeployment();
 
-let baseMachine = new Machine({
-    provider: 'Amazon',
-    cpu: new Range(1),
-    ram: new Range(2),
-    sshKeys: githubKeys('ejj'),
+const baseMachine = new Machine({
+  provider: 'Amazon',
+  cpu: new Range(1),
+  ram: new Range(2),
+  sshKeys: githubKeys('ejj'),
 });
 namespace.deploy(baseMachine.asMaster());
 namespace.deploy(baseMachine.asWorker().replicate(3));
 
-let mongo = new Mongo(3);
-let app = new Node({
+const mongo = new Mongo(3);
+const app = new Node({
   nWorker: 3,
   repo: 'https://github.com/quilt/node-todo.git',
   env: {
@@ -26,7 +26,11 @@ let app = new Node({
   },
 });
 
-let proxy = haproxy.singleServiceLoadBalancer(3, app._app);
+// The Node module isn't designed well for composition right now, so we have
+// to access app._app directly. Until this is fixed, silence the linter
+// warning about underscore dangles.
+// eslint-disable-next-line no-underscore-dangle
+const proxy = haproxy.singleServiceLoadBalancer(3, app._app);
 
 // Places all haproxy containers on separate Worker VMs.
 // This is just for convenience for the example instructions, as it allows us to
